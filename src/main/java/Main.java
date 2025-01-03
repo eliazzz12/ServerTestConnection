340db0cf9d5265a863ef6f1a39d355db7a3e2832
Elias Martín González <70772713+eliazzz12@users.noreply.github.com>
2025-01-03 18:00:51 +0100

    Recibir y enviar mensajes

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    private static final int PORT = 6200;
+    public static void main(String[] args) {
+        try(ServerSocket serverSocket = new ServerSocket(PORT)) {
+            Socket client = serverSocket.accept();
+            DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+            DataInputStream dis = new DataInputStream(client.getInputStream());
+
+            dos.writeUTF("Saludos desde la Raspberry Pi de la Secta Mermelada :D");
+
+            String input = dis.readUTF();
+
+            dos.writeUTF("Mensaje recibido: " +input);
+
+        } catch (IOException e) {
+            System.out.println("SERVER Status: Stopped");
+        }
+    }
+}

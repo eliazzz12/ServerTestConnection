df4b92c1e7419ef84142710800240dab38075568
Elias Martín González <70772713+eliazzz12@users.noreply.github.com>
2025-01-07 18:57:55 +0100

    Añadir información antes del bucle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,31 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Main {
     private static final int PORT = 10100;
     public static void main(String[] args) {
+        System.out.println("Iniciando programa test");
+
+        try {
+            System.out.println("========================================================");
+            InetAddress dir = InetAddress.getByName("sectamermelada.ddns.net");
+
+            // Array de tipo InetAddress con todas las direcciones IP
+            //asignadas a sectamermelada.ddns.net
+            System.out.println("\tDIRECCIONES IP PARA: " + dir.getHostName());
+            InetAddress[] direcciones =
+                    InetAddress.getAllByName(dir.getHostName());
+            for (InetAddress dirs : direcciones) System.out.println("\t\t" + dirs.toString());
+
+            System.out.println("========================================================");
+        } catch (UnknownHostException e) {
+            System.out.println("Error en la conexión al servidor");
+        }
         while(true){
             try(ServerSocket serverSocket = new ServerSocket(PORT)) {
                 Socket client = serverSocket.accept();

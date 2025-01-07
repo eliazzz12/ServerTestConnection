aea95cc4b0f99b8e02bf3aa8e066255073ce094f
Elias Martín González <70772713+eliazzz12@users.noreply.github.com>
2025-01-07 13:29:37 +0100

    Bucle infinito, cambiar mensajes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,19 +7,22 @@ import java.net.Socket;
 public class Main {
     private static final int PORT = 10100;
     public static void main(String[] args) {
-        try(ServerSocket serverSocket = new ServerSocket(PORT)) {
-            Socket client = serverSocket.accept();
-            DataOutputStream dos = new DataOutputStream(client.getOutputStream());
-            DataInputStream dis = new DataInputStream(client.getInputStream());
+        while(true){
+            try(ServerSocket serverSocket = new ServerSocket(PORT)) {
+                Socket client = serverSocket.accept();
+                DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+                DataInputStream dis = new DataInputStream(client.getInputStream());
 
-            dos.writeUTF("Saludos desde la Raspberry Pi de la Secta Mermelada :D");
+                dos.writeUTF("SERVER: Saludos desde la Raspberry Pi de la Secta Mermelada :D");
 
-            String input = dis.readUTF();
+                String input = dis.readUTF();
 
-            dos.writeUTF("Mensaje recibido: " +input);
+                dos.writeUTF("SERVER: Mensaje recibido= " +input);
 
-        } catch (IOException e) {
-            System.out.println("SERVER Status: Stopped");
+            } catch (IOException e) {
+                System.out.println("SERVER Status: Stopped by Exception");
+            }
+            System.out.println("SERVER Status: Finished");
         }
     }
 }
